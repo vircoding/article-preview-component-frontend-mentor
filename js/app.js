aaeb1e5dc99fe0ef87cd3df498ffecd4b6a1c8e7
PVIdubs <nluismi80@gmail.com>
2022-10-11 10:54:13 -0400

    mobile animation

--- js/app.js
+++ js/app.js
@@ -1,57 +1,28 @@
-function drawAnimation(element) {
-    // while timePassed goes from 0 to 2000
-    // left gets values from 0px to 400px
-    element.style.right = timePassed / 5 + 'px';
+function unhideShare() {
+    path.style.fill = 'hsl(210, 46%, 95%)';
+    shareIcon.classList.add('iconActive');
+    animate('unhide', 'ease-out');
 }
 
-function unhideShare(element) {
-    let start = Date.now(); // remember start time
-    let timer = setInterval(() => {
-        // How long has it been since the beginning?
-        let timePassed = Date.now() - start;
-
-        if (timePassed >= 2000) {
-            clearInterval(timer); // end animation after 2 seconds
-            return;
-        }
-
-        // draw the animation at timePassed
-        element.style.left = timePassed / 5 + 'px';
-    }, 20);
+function hideShare() {
+    path.style.fill = 'hsl(214, 17%, 51%)';
+    shareIcon.classList.remove('iconActive');
+    animate('hide', 'ease-in');
 }
 
-function hideShare(element) {
-    let start = Date.now(); // remember start time
-    let timer = setInterval(() => {
-        // How long has it been since the beginning?
-        let timePassed = Date.now() - start;
-
-        if (timePassed >= 2000) {
-            clearInterval(timer); // end animation after 2 seconds
-            return;
-        }
-
-        // draw the animation at timePassed
-        element.style.right = timePassed / 5 + 'px';
-    }, 20);
+function animate(animation, timing) {
+    const element = document.querySelector('.footer-share');
+    element.style.animationTimingFunction = timing;
+    element.style.animationName = animation;
 }
-
-document.querySelector('.footer-share').style.right = '396.2px';
-const shareIcon = document.getElementById('shareIcon');
 
 shareIcon.addEventListener('load', () => {
     const svgDoc = shareIcon.contentDocument;
-    const path = svgDoc.firstElementChild.firstElementChild;
+    path = svgDoc.firstElementChild.firstElementChild;
 
-    document.querySelector('.user-iconContainer').addEventListener('click', () => {
-        if (shareIcon.classList.contains('iconActive')) {
-            path.style.fill = 'hsl(214, 17%, 51%)';
-            shareIcon.classList.remove('iconActive');
-            hideShare(document.querySelector('.footer-share'));
-        } else {
-            path.style.fill = 'hsl(210, 46%, 95%)';
-            shareIcon.classList.add('iconActive');
-            unhideShare(document.querySelector('.footer-share'));
-        }
-    });
+    document
+        .querySelector('.user-iconContainer')
+        .addEventListener('click', () =>
+            shareIcon.classList.contains('iconActive') ? hideShare() : unhideShare()
+        );
 });
